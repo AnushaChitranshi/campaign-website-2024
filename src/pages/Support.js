07e1d789e4918c5dc3f06a690d5c8b44bfc64a52
Anusha Chitranshi <chitraaa@mail.uc.edu>
2024-02-24 14:07:30 -0500

    feat(client): added supporters

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { FloatButton } from 'antd';
-
+import Support1 from "../images/Support-1.png"
+import Support2 from "../images/Support-2.png"
+import Support3 from "../images/Support-3.png"
+import Support4 from "../images/Support-4.png"
+import Support5 from "../images/Support-5.png"
+import Support6 from "../images/Support-6.png"
 
 const Support = () => {
     return (
@@ -10,16 +15,12 @@ const Support = () => {
             </div>
             <h2>OUR SUPPORTERS</h2>
             <div className="support-grid">
-                {/* <img className="support-card" src={Abigail} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Alex} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Andrea} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Austin} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Camryn} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Hannah} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Jack} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Neha} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Rebekah} alt="Testimonial for Taylor and Vu"/>
-                <img className="support-card" src={Temple} alt="Testimonial for Taylor and Vu"/> */}
+                <img className="support-card" src={Support1} alt="Testimonial"/>
+                <img className="support-card" src={Support2} alt="Testimonial"/>
+                <img className="support-card" src={Support3} alt="Testimonial"/>
+                <img className="support-card" src={Support4} alt="Testimonial"/>
+                <img className="support-card" src={Support5} alt="Testimonial"/>
+                <img className="support-card" src={Support6} alt="Testimonial"/>
             </div>
         </div>
     )
